f9343a1ccb3583adb65acad3978ed3e2e3d1b513
Ubuntu <ubuntu@ip-10-0-1-232.ap-south-1.compute.internal>
2020-11-23 11:36:02 +0000

    modified

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Catss 🐺")
-option_b = os.getenv('OPTION_B', u"Dogss 🐶")
+option_a = os.getenv('OPTION_A', u"Catsssss 🐺")
+option_b = os.getenv('OPTION_B', u"Dogssssss 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
